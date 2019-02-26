20b58dc079fcd880e9c5a00be08dad249b89a4f2
810209659@qq.com <810209659@qq.com>
2019-02-26 14:03:54 +0800

    修改java对象转json 依赖包

--- src/com/rong/ssi/common/SessionFilter.java
+++ src/com/rong/ssi/common/SessionFilter.java
@@ -10,7 +10,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang.StringUtils;
 /**
  * @author 容培淼
  *Session工具类，在web.xml中配置监听action请求，过滤异常请求
